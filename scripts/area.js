56a253c3d10878c41509657485e34454527d4fcf
RBCodeJourney <refatullahbiswas@gmail.com>
2023-10-18 03:40:25 +0530

    Ellipse calculator added

--- scripts/area.js
+++ scripts/area.js
@@ -6,7 +6,6 @@ function calculateTriangleArea() {
     const base = parseFloat(baseValueText);
     console.log(base);
 
-
     // get Triangle height value
     const heightField = document.getElementById('triangle-height');
     const heightValueText = heightField.value;
@@ -18,7 +17,6 @@ function calculateTriangleArea() {
     // show triangle area 
     const areaSpan = document.getElementById('triangle-area');
     areaSpan.innerText = area;
-
 }
 
 function calculateRectangleArea() {
@@ -35,11 +33,47 @@ function calculateRectangleArea() {
     console.log(length);
 
     // calculate area
-    const area =  width * length;
-
-
+    const area = width * length;
 
     // show rectangle are
     const rectangleAreaSpan = document.getElementById('rectangle-area');
     rectangleAreaSpan.innerText = area;
+};
+
+
+// reusable function ----> reduce duplicate code
+function calculateParallelogramArea() {
+    const base = getInputValue('parallelogram-base');
+    // console.log(base);
+
+    const height = getInputValue('parallelogram-height');
+    // console.log(height);
+
+    const area = base * height;
+    setElementInnerText('parallelogram-area', area);
+}
+
+function calculateEllipseArea() {
+    const majorRadius = getInputValue('ellipse-major-radius');
+    const minorRadius = getInputValue('ellipse-minor-radius');
+    const area = 3.14 * majorRadius * minorRadius;
+    // const areaTwoDecimal = area.toFixed(2);
+    setElementInnerText('ellipse-area', area);
+}
+
+// reusable get input value field in number
+function getInputValue(fieldId) {
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+
+// reusable set span, p, div, etc text
+
+function setElementInnerText(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+
+
 }
